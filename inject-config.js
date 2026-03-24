bfe4106d5bb7484de24872aa65680a5ca09ec1c5
wahukiragu <wahuconsults@gmail.com>
2026-03-25 00:32:27 +0300

    Create inject-config.js

---
+++ inject-config.js
@@ -0,0 +1,5 @@
+// This file is intentionally empty in the repo.
+// Netlify will replace it with real values at deploy time.
+// DO NOT put real keys here.
+window.__NM_PK__     = '';
+window.__NM_ZAPIER__ = '';
