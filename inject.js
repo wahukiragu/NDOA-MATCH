7121ff16f19603ce7629b13bbea41c7d50ad2328
wahukiragu <wahuconsults@gmail.com>
2026-03-25 01:05:47 +0300

    Update inject.js

--- inject.js
+++ inject.js
@@ -5,4 +5,5 @@ const zapier = process.env.NM_ZAPIER || '';
 
 const content = `window.__NM_PK__ = '${pk}';\nwindow.__NM_ZAPIER__ = '${zapier}';\n`;
 fs.writeFileSync('inject-config.js', content);
-console.log('Config injected successfully.');
+console.log('Config injected.');
+```
